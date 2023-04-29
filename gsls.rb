6cd1f2b13951f11ce9cf8843e793526446c2aba2
John Stilia <stilia.johny@gmail.com>
2023-04-30 00:43:10 +0100

    Update gsls.rb
    Signed-off-by: John Stilia <stilia.johny@gmail.com>

--- gsls.rb
+++ gsls.rb
@@ -1,7 +1,7 @@
 class Gsls < Formula
   desc "Get a quick overview of Git repositories with gs, a alternative to ls"
-  homepage "https://github.com/non-existing-organization/golang-gsls"
-  url "https://github.com/non-existing-organization/golang-gsls/releases/download/v0.1.1/golang-gsls-0.1.1.tar.gz"
+  homepage "https://github.com/homebrew-non-existing-organization/golang-gsls"
+  url "https://github.com/non-existing-organization/homebrew-golang-gsls/releases/download/v0.1.1/golang-gsls-0.1.1.tar.gz"
   version "0.1.1"
   sha256 "e94324cac0245158ce8e1a9337f3b9a5431e513f9cc2c4d7641773f698bede3a"
   license "Unlicense"
