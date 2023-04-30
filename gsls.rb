5f5956930c863b3aa4f460b843c30e2846dc4db7
John Stilia <stilia.johny@gmail.com>
2023-04-30 17:10:04 +0100

    Update gsls.rb
    Signed-off-by: John Stilia <stilia.johny@gmail.com>

--- gsls.rb
+++ gsls.rb
@@ -3,7 +3,7 @@ class Gsls < Formula
   homepage "https://github.com/homebrew-non-existing-organization/golang-gsls"
   url "https://github.com/non-existing-organization/homebrew-golang-gsls/releases/download/v0.1.1/golang-gsls-0.1.1.tar.gz"
   version "0.1.1"
-  sha256 "e94324cac0245158ce8e1a9337f3b9a5431e513f9cc2c4d7641773f698bede3a"
+  sha256 "7eccf0ccc7981826b3e0360cd74e4a1fc79b79ce18061fba4bc9311cd3c00b12"
   license "Unlicense"
 
   def install
