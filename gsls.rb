eca1a436f16ecce8bbea0f9c031c7af29a6a6b27
John Stilia <stilia.johny@gmail.com>
2023-05-01 10:26:18 +0100

    fix version on brew file

--- gsls.rb
+++ gsls.rb
@@ -1,7 +1,7 @@
 class Gsls < Formula
   desc "Get a quick overview of Git repositories with gs, a alternative to ls"
   homepage "https://github.com/non-existing-organization/golang-gsls"
-  url "https://github.com/non-existing-organization/golang-gsls/releases/download/v0.2.0/golang-gsls-0.2,0.tar.gz"
+  url "https://github.com/non-existing-organization/golang-gsls/releases/download/v0.2.0/golang-gsls-0.2.0.tar.gz"
   version "0.2.0"
   sha256 "6276da2b5b646d703ed1c4874720e521401b2fdc74c47991e3be5a061ee76968"
   license "Unlicense"
